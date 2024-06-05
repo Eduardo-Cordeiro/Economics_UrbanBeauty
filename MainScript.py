a1df1189e1b367fc2ab6209d4e01e63ffd99f684
Eduardo Cordeiro <56442919+Eduardo-Cordeiro@users.noreply.github.com>
2024-06-04 22:19:55 -0300

    Update MainScript.py
    "Building the area and centers of the neighborhoods"

--- MainScript.py
+++ MainScript.py
@@ -11,11 +11,9 @@ data_parques = pd.read_excel("C:\\Users\\eduar\\Desktop\\GitHubProjects\\TCC\\Da
 
 data = data.sort_values(by="NOME")
 
-print(data["NOME"])
-
 bairros = list(set(data["NOME"].to_list()))
 bairros.sort()
-print(bairros)
+
 
 def selectcord_bairro(bairro,df,cord):
 
@@ -29,20 +27,58 @@ def area_calc(x,y):
     nx = len(x)
     soma = []
     somb = []
-    count = -1
+    count = 0
     for i in x:
-        count = count + 1
-
-        ai = (x[count]*y[count+1] + x[-1]*y[0])
-        soma.append(ai)
-        
-        bi = (y[count]*x[count+1] + y[-1]*x[0])
-        somb.append(bi)
-
-        return (sum(soma) - sum(somb))*0.5
+        while count < nx-1:
+            ai = (x[count]*y[count+1] + x[-1]*y[0])
+            soma.append(ai)
+            
+            bi = (y[count]*x[count+1] + y[-1]*x[0])
+            somb.append(bi)
+            count += 1
+    return (sum(soma) - sum(somb))*0.5
 
 teste = area_calc(selectcord_bairro(bairros[0],data,"x"),selectcord_bairro(bairros[0],data,"y"))
 
-print(teste)
+def calc_center(x,y,cord):
+    nx = len(x)
+    soma = []
+    somb = []
+    count = 0
+    for i in x:
+        while count < nx-1:
+            ai = (x[count]*y[count+1] + x[-1]*y[0])
+            soma.append(ai)
+            
+            bi = (y[count]*x[count+1] + y[-1]*x[0])
+            somb.append(bi)
+            count += 1
+    a = (sum(soma) - sum(somb))*0.5
+    soma = []
+    count = 0
 
+    if cord == 'x':
+        for i in x: 
+            while count < nx-1:
+                soma.append((x[count]+x[count+1])*((x[count]*y[count+1]) - (x[count+1]*y[count])))
+                count += 1
+        return sum(soma)/(6*a)
+    elif cord == 'y':
+        for i in x:
+            while count < nx-1:
+                soma.append((y[count]+y[count+1])*((x[count]*y[count+1]) - (x[count+1]*y[count])))
+                count += 1
+        return sum(soma)/(6*a)
+        
+lat_centro = []
+long_centro = []
+for i in bairros:
+    lat_centro.append(calc_center(selectcord_bairro(i,data,"x"),selectcord_bairro(i,data,"y"),'x'))
+    long_centro.append(calc_center(selectcord_bairro(i,data,"x"),selectcord_bairro(i,data,"y"),'y'))
+    
+data_tratada = pd.DataFrame()
+data_tratada["BAIRROS"] = bairros
+data_tratada["LAT_CENTRO"] = lat_centro
+data_tratada["LONG_CENTRO"] = long_centro
 
+print(data_tratada)
