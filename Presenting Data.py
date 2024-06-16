8f4767b4a0306f6612683d0a7331772f854ca94e
Eduardo Cordeiro <56442919+Eduardo-Cordeiro@users.noreply.github.com>
2024-06-16 16:27:32 -0300

    Update Presenting Data.py

--- Presenting Data.py
+++ Presenting Data.py
@@ -10,18 +10,36 @@ macrozones = pd.read_csv("C:\\Users\\escordeiro\\Downloads\\Data\\Data\\data_mac
 #Pining center with Folium
 m = folium.Map(location=(-30, -51))
 
+Macrozones_Group = folium.FeatureGroup(name='Macrozones')
+Neighborhoods_Group = folium.FeatureGroup(name='Neighborhoods')
+
+small_cities = folium.FeatureGroup(name='Small Cities')
+
 for i in macrozones["MACROZONE"]:
-    folium.Marker(
+    a = folium.CircleMarker(
         location = (macrozones.loc[macrozones["MACROZONE"] == i,"LAT_CENTER"].values[0], macrozones.loc[macrozones["MACROZONE"] == i,"LONG_CENTER"].values[0]),
         popup = i,
-        icon=folium.Icon(color='blue', icon='info-sign')
-    ).add_to(m)
+        radius = (macrozones.loc[macrozones["MACROZONE"] == i,'HGI_GEOM_CENTER'].values[0]/100),
+        color='blue',
+        fill=True,
+        fill_color='blue',
+        fill_opacity=0.6,
+        )
+    a.add_to(Macrozones_Group)
 
 for i in neighborhoods["NEIGHBORHOOD"]:
-    folium.Marker(
+    b = folium.CircleMarker(
         location = (neighborhoods.loc[neighborhoods["NEIGHBORHOOD"] == i,"LAT_CENTER"].values[0], neighborhoods.loc[neighborhoods["NEIGHBORHOOD"] == i,"LONG_CENTER"].values[0]),
         popup = i,
-        icon=folium.Icon(color='red', icon='info-sign')
-    ).add_to(m)
+        radius = (neighborhoods.loc[neighborhoods["NEIGHBORHOOD"] == i,'HGI_GEOM_CENTER'].values[0]/100),
+        color='red',
+        fill=True,
+        fill_color='red',
+        fill_opacity=0.6,
+        )
+    b.add_to(Neighborhoods_Group)
 
+Macrozones_Group.add_to(m)
+Neighborhoods_Group.add_to(m)
+folium.LayerControl().add_to(m)
 m.save("index.html")
